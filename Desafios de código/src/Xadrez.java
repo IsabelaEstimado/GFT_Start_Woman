bedb232c263b5d9c17eec3f26ee24618186f92bc
IsabelaEstimado <isabelaestimado@gmail.com>
2022-05-30 15:53:24 -0300

    repositorio bootcamp

---
+++ Desafios de código/src/Xadrez.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Xadrez {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int L = sc.nextInt();
+            int C = sc.nextInt();
+            if ((L%2!=0)&& (C%2!=0)) //se as casas são ambas ímpares
+                System.out.println("1"); //branco
+            else
+                System.out.println("0"); //preto
+            sc.close();
+
+        }
+}
